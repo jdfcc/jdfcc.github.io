14c39b763b81c0cabf0f0fc8a0b7cd7c8160be2c
jdfcc <3039898075@qq.com>
2025-10-09 15:05:06 +0800

    fix: rm scirpt

--- _plugins/default_date.rb
+++
@@ -1,6 +0,0 @@
-# _plugins/default_date.rb
-Jekyll::Hooks.register :posts, :pre_render do |post, payload|
-  if post.data['date'].nil?
-    post.data['date'] = Time.new(2025, 1, 1)  # 默认时间，可改为 Time.now
-  end
-end
