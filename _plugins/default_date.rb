7ff5244557d11b25818d17eff278da4e57828b70
jdfcc <3039898075@qq.com>
2025-10-09 13:48:22 +0800

    test

--- _plugins/default_date.rb
+++ _plugins/default_date.rb
@@ -1,7 +1,5 @@
-# _plugins/default_date.rb
-Jekyll::Hooks.register :posts, :pre_render do |post, payload|
+Jekyll::Hooks.register :posts, :post_read do |post|
   if !post.data['date'] || post.data['date'].nil?
-    # 默认日期，例如 2025-01-01
     post.data['date'] = Time.new(2025, 1, 1)
   end
 end
